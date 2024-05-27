03dcc16103f066eed13199cac79078fb8ec2ee9f
kenokasa <kenokasa2035@gmail.cim>
2024-05-26 19:02:06 -0700

    $Message

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""function that returns the list of available attributes and methods of an object"""
+"""function that returns the list of availab
+le attributes and methods of an object"""
+
+
 def lookup(obj):
     """ function to return the list """
     return(dir(obj))
