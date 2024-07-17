181d7b36b916ef80675fe11320868cabbe307141
kenokasa <Kenokasa2035@gmail.com>
2024-07-17 16:28:32 -0700

    standardaize

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,13 +3,13 @@
 const { argv } = require('node:process');
 
 if (parseInt(argv[2])) {
-	    for (let i = 0; i < parseInt(argv[2]); i++) {
-		    let keno = '';
-		            for (let j = 0; j < parseInt(argv[2]); j++) {
-				                keno += 'X';
-				            }
-		           console.log(keno);
-		          }
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    let keno = '';
+    for (let j = 0; j < parseInt(argv[2]); j++) {
+      keno += 'X';
+    }
+    console.log(keno);
+  }
 } else {
-	    console.log('Missing size');
+  console.log('Missing size');
 }
