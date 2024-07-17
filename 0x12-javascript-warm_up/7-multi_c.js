887e3fcb6648ff2b9ed89de859cd4e2c74eeb0d6
kenokasa <Kenokasa2035@gmail.com>
2024-07-17 15:54:35 -0700

    combination

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 
 const { argv } = require('node:process');
 if (parseInt(argv[2])) {
-    const keno = parseInt(argv[2]);
-    for (var i in keno) {
-        console.log('C is fun');
-    }
-  } else {
-    console.log('Not a number');
+  const keno = parseInt(argv[2]);
+  for (let i = 0; i < keno; i++) {
+    console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
+}
