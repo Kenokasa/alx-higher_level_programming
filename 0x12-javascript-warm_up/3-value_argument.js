f550767056b420f1cf6065cc0f2a0e2ab80a112c
kenokasa <Kenokasa2035@gmail.com>
2024-07-17 14:52:45 -0700

    hot fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,8 @@
 
 const { argv } = require('node:process');
 
-if (argv[2])
-{
-    console.log(argv[2]);
+if (argv[2]) {
+  console.log(argv[2]);
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
