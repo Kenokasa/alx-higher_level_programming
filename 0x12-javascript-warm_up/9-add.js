d6d05f98e13d49f72cd3f825184892812ab63c41
kenokasa <kenokasa2035@gmail.com>
2024-07-22 10:40:08 -0900

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,5 +5,8 @@ const { argv } = require('node:process');
 function add (a, b) {
   return a + b;
 }
-
-console.log(add(parseInt(argv[2]), parseInt(argv[3])));
+if (parseInt(argv[2]) && parseInt(argv[3])) {
+  console.log(add(parseInt(argv[2]), parseInt(argv[3])));
+} else {
+  console.log('NaN');
+}
