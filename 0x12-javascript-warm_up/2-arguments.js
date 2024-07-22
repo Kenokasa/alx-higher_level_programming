0ed9e6659ee7b5d3c87d31a7a64380d52cd4f750
kenokasa <kenokasa2035@gmail.com>
2024-07-22 10:23:45 -0900

    quick

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-const args = process.argv.slice(2);
+const { argv } = require('node:process');
 
 if (args.length === 0) {
-  console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
+    console.log("No argument");
+  } else if (args.length === 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+  }
