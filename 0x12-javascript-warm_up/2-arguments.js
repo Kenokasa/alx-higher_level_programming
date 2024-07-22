df09a7d51cee628dc33afaaf164b09db746a503c
kenokasa <kenokasa2035@gmail.com>
2024-07-22 10:04:13 -0900

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,8 @@ const { argv } = require('node:process');
 
 if (argv.length === 2) {
     console.log('Argument found');
-}if (argv.length > 2) {
+}
+if (argv.length > 2) {
     console.log('Arguments found');
 } else {
     console.log('No argument');
