06fcf5b6636e19ec8e950779f7c87b9d721c6e4b
kenokasa <kenokasa2035@gmail.com>
2024-07-24 09:48:11 -0900

    first table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- description of frist_table from hbtn_0d_usa
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SELECT * FROM first_table
